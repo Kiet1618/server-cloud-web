2d1d9c808a73a19d22f03c9acbb64d12826e57e6
Kiet1618 <ngokiet3207@gmail.com>
2023-10-17 16:49:25 +0700

    update

--- app.js
+++ app.js
@@ -16,6 +16,13 @@ app.use(express.json());
 app.use(cookieParser());
 app.use(bodyParser.urlencoded({ extended: true }));
 app.use(fileUpload());
+app.use((req, res, next) => {
+    res.header('Access-Control-Allow-Origin', 'http://localhost:3000');
+    res.header('Access-Control-Allow-Origin', 'https://flipkart.myvnc.com');
+    res.header('Access-Control-Allow-Methods', 'GET, POST, PUT, DELETE');
+    res.header('Access-Control-Allow-Headers', 'Content-Type');
+    next();
+});
 
 const user = require('./routes/userRoute');
 const product = require('./routes/productRoute');
